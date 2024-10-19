b225b91bdf497fbef96cac36f61242a8c0b809dd
Arkadyuti Chaudhuri <Arkadyutichaudhuri@gmail.com>
2024-10-19 22:29:44 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import About from './components/About'
+import Domain from './components/Domain'
+
+const App = () => {
+  return (
+    <>
+    
+    <Navbar/>
+    <Home/>
+    <About/>
+    <Domain/>
+    </>
+  )
+}
+
+export default App
